00881eedb9496c301594cfc2c3c0cb8b4e4a6806
pjmap <pjmap@proton.me>
2026-05-08 20:00:20 -0500

    Added word counting.

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ words = text.split()
 # Displays total word count.
 print(f"Total # of words: {len(words)}")
 
-# count recurring words
+# TLDR: count recurring words
+#
+word_counts = {}
+
+for term in words:
+    if term in word_counts:
+        word_counts[term] += 1
+    else:
+        word_counts[term] = 1
+
+print(word_counts)
+
 # filter common filler words
 # print most common terms
